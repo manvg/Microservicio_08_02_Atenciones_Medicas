9d28a77ace25301668f2902e9f03d34e241a2c9b
Manuel Valdés <manuelvaldesg97@gmail.com>
2024-04-12 00:21:23 -0400

    Creación proyecto microservicio atenciones

---
+++ src/main/java/com/crud/atenciones/AtencionesApplication.java
@@ -0,0 +1,13 @@
+package com.crud.atenciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtencionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtencionesApplication.class, args);
+	}
+
+}
